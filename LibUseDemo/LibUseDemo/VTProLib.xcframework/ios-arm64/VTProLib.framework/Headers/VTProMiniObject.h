25526fc6acd7c80d9b2d62e257f49c890f9a43af
yangweichao <yangweichao@lepucloud.com>
2024-01-22 19:47:01 +0800

    update: update

--- LibUseDemo/LibUseDemo/VTProLib.xcframework/ios-arm64/VTProLib.framework/Headers/VTProMiniObject.h
+++ LibUseDemo/LibUseDemo/VTProLib.xcframework/ios-arm64/VTProLib.framework/Headers/VTProMiniObject.h
@@ -12,20 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 /// @brief Minimonitor  Packet data   --  1 pcs  /40ms.    A part of data not support at current version. 
 @interface VTProMiniObject : NSObject
 
+@property (nonatomic, strong) NSData *originData;
+
 /// @brief ECG data with real-time data
 @property (nonatomic, strong) NSMutableArray *ecgArray;
-
-/// @brief SpO2 data with real-time data
-@property (nonatomic, strong) NSMutableArray *spo2Array;
-
-/// @brief SpO2 Value
-@property (nonatomic, assign) u_char spo2Value;
-
-/// @brief SpO2 - PI Value
-@property (nonatomic, assign) u_char spo2PIValue;
-
-/// @brief SpO2 - PR Value
-@property (nonatomic, assign) u_short spo2PRValue;
 
 /// @brief ECG - HR Value
 @property (nonatomic, assign) u_short hrValue;
@@ -39,14 +29,26 @@ NS_ASSUME_NONNULL_BEGIN
 /// @brief ECG - PVCs Value
 @property (nonatomic, assign) u_short pvcValue;
 
-/// @brief battery of peripheral
-@property (nonatomic, assign) u_char battery;
+/// @brief ECG - R wave marker
+@property (nonatomic, assign) u_char ecgRIdentifier;
+
+/// @brief SpO2 data with real-time data
+@property (nonatomic, strong) NSMutableArray *spo2Array;
+
+/// @brief SpO2 - PR Value
+@property (nonatomic, assign) u_short spo2PRValue;
+
+/// @brief SpO2 Value
+@property (nonatomic, assign) u_char spo2Value;
+
+/// @brief SpO2 - PI Value
+@property (nonatomic, assign) u_char spo2PIValue;
 
 /// @brief SpO2 - PR marker
 @property (nonatomic, assign) u_char spo2Identifier;
 
-/// @brief ECG - R wave marker
-@property (nonatomic, assign) u_char ecgRIdentifier;
+/// @brief battery of peripheral
+@property (nonatomic, assign) u_char battery;
 
 /// @brief Packet number .  A part of version is not support.
 @property (nonatomic, assign) u_char pkgNum;
